fede9daad419b629e4b2ad5f1cf7ea360aa736fd
Tony <pbach@macalester.edu>
2014-11-01 23:59:24 -0500

    first commit

---
+++ js/dummyData.js
@@ -0,0 +1,20 @@
+// <![CDATA[
+$(document).ready(function(){
+ 
+//set up markers
+	var myMarkers = {"markers": [
+	{"latitude": "31.42866311735861", "longitude":"-98.61328125", "icon": "img/blue_pin_2.png", "baloon_text": 'This is <strong>Texas</strong>'},
+	{"latitude": "35.101934057246055", "longitude":"-96.6796875", "icon": "img/orange_pin_2.png", "baloon_text": 'This is <strong>Oklahoma</strong>'},
+	{"latitude": "38.61687046392973", "longitude":"-98.876953125", "icon": "img/blue_pin_2.png", "baloon_text": 'This is <strong>Kansas</strong>'}
+	]
+	};
+	 
+	//set up map options
+	$("#map").mapmarker({
+	zoom : 5,
+	center : 'United States',
+	markers : myMarkers
+	});
+	 
+});
+// ]]
